f09f45932727694f4c8a4aeee670b6066882c701
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-08-17 01:35:33 +0100

    Added es6 subclasses

--- oop_section/app.js
+++ oop_section/app.js
@@ -246,3 +246,31 @@
 // mary.getsMarried("O'Connor");
 
 // console.log(mary.greeting());
+
+// SUB CLASSES ==============================================================
+
+// class Person {
+//     constructor(firstName, lastName){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+//     greeting(){
+//         return `Hello there ${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// class Customer extends Person {
+//     constructor(firstName, lastName, phone, membership){
+//         super(firstName, lastName);
+
+//         this.phone = phone;
+//         this.membership = membership;
+//     }
+//     static getMembershipCost(){
+//         return 500;
+//     }
+// }
+
+// const john = new Customer('John', 'Doe', '000-555-444', 'standard');
+
+// console.log(Customer.getMembershipCost());
