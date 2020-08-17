d1e73d77093936f4e699324adab736dd6cce686b
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-08-17 01:10:59 +0100

    Added es6 classes and constructors

--- oop_section/app.js
+++ oop_section/app.js
@@ -216,3 +216,33 @@
 // */ 
 
 // console.log(Elias)
+
+// ES6 CLASSES ==============================================================
+// class Person {
+//     constructor(firstName, lastName, dob){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.birthday = new Date(dob);
+//     }
+//     greeting(){
+//         return `Hello there ${this.firstName} ${this.lastName}`;
+//     }
+//     calculateAge(){
+//         const diff = Date.now() - this.birthday.getTime();
+//         const ageDate = new Date(diff);
+//         return Math.abs(ageDate.getUTCFullYear() - 1970);
+//     }
+//     getsMarried(newLastName){
+//         this.lastName = newLastName;
+//     }
+//     // just a example
+//     static addNumbers(x,y){
+//         x + y;
+//     }
+// }
+
+// const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+// mary.getsMarried("O'Connor");
+
+// console.log(mary.greeting());
