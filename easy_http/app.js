291cbcf0da5a687f48a00bcce8362e7fcd8af0e8
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-09-07 00:16:10 +0100

    added delete and put request

--- easy_http/app.js
+++ easy_http/app.js
@@ -1,12 +1,49 @@
 
 const http = new easyHTTP;
 
-//get posts
-http.get('https://jsonplaceholder.typicode.com/posts', 
-function(error, posts){
+// //get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', 
+// function(error, posts){
+//     if(error){
+//         console.log(error)
+//     }else{
+//         console.log(posts);
+//     }
+// });
+
+//Create data
+
+const data = {
+    title: 'Custom post',
+    body: 'Content here'
+};
+
+// create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, 
+// function(error, post){
+//     if(error){
+//         console.log(error)
+//     }else{
+//         console.log(post);
+//     }
+// });
+
+//Update post
+http.put('https://jsonplaceholder.typicode.com/posts/5', data, 
+function(error, post){
     if(error){
         console.log(error)
     }else{
-        console.log(posts);
+        console.log(post);
+    }
+});
+
+//delete posts
+http.delete('https://jsonplaceholder.typicode.com/posts/1', 
+function(error, response){
+    if(error){
+        console.log(error)
+    }else{
+        console.log(response);
     }
 });
