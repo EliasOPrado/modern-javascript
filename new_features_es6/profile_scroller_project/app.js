f364f12290fbeb3cf8bb6910ab91c3cc56b876aa
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-09-24 00:27:54 +0100

    added data and functionality

--- new_features_es6/profile_scroller_project/app.js
+++ new_features_es6/profile_scroller_project/app.js
@@ -5,12 +5,12 @@ const data = [
         gender: 'male',
         lookingfor: 'female',
         location: 'Booston MA',
-        image: 'https://randomuser.me/api/portraits/men/82.jpg'
+        image: 'https://randomuser.me/api/portraits/men/81.jpg'
     },
     {
-        name: 'Jen Smith',
+        name: 'Jean Smith',
         age: 26,
-        gender: 'female',
+        gender: 'Male',
         lookingfor: 'male',
         location: 'Miami FL',
         image: 'https://randomuser.me/api/portraits/men/82.jpg'
@@ -23,4 +23,48 @@ const data = [
         location: 'Lynn',
         image: 'https://randomuser.me/api/portraits/men/83.jpg'
     },
-]
+];
+
+const profiles = profileIterator(data);
+
+// call first profiles
+nextProfile();
+
+// next event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// next profile display
+function nextProfile(){
+    const currentProfile = profiles.next().value;
+
+    if(currentProfile !== undefined){
+    document.getElementById('profileDisplay').innerHTML = `
+        <ul class="list-group">
+            <li class="list-group-item">Name: ${currentProfile.name}</li>
+            <li class="list-group-item">Age: ${currentProfile.age}</li>
+            <li class="list-group-item">Gender: ${currentProfile.gender}</li>
+            <li class="list-group-item">Preference: ${currentProfile.lookingfor}</li>
+            <li class="list-group-item">Location: ${currentProfile.location}</li>
+        </ul>
+    `;
+    document.getElementById('imageDisplay').innerHTML = `
+        <img src="${currentProfile.image}">
+    `;
+    } else {
+        // no more profiles
+        window.location.reload();
+    }
+}
+
+// profile iterator
+function profileIterator(profiles){
+    let nextIndex = 0;
+
+    return {
+            next: function(){
+                return nextIndex < profiles.length ? {value : profiles[nextIndex++],
+                done: false} : 
+                {done: true}
+        }
+    };
+}
